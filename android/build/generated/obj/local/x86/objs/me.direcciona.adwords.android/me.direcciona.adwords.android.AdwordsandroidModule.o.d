e5e2668c35d023f4928b010a1beb24d55f040843
Mojo-Tribe <mojotribe@Muhamads-MacBook-Pro.local>
2017-06-06 11:55:03 +0300

    * Bump version * Updated to Titanium 6.x

---
+++ android/build/generated/obj/local/x86/objs/me.direcciona.adwords.android/me.direcciona.adwords.android.AdwordsandroidModule.o.d
@@ -0,0 +1,161 @@
+/Users/mojotribe/Documents/Carrefour2018Main/AdWordsAndroid/android/build/generated/obj/local/x86/objs/me.direcciona.adwords.android/me.direcciona.adwords.android.AdwordsandroidModule.o: \
+ jni/me.direcciona.adwords.android.AdwordsandroidModule.cpp \
+ jni/me.direcciona.adwords.android.AdwordsandroidModule.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/Proxy.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/v8.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/stdint.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/stdio.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/wchar.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/wctype.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/xlocale.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/utility \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__config \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__tuple \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/cstddef \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/type_traits \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/vector \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__bit_reference \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/algorithm \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/initializer_list \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/cstring \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/string.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/memory \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/typeinfo \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/exception \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/cstdint \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/new \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/limits \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/iterator \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__functional_base \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/iosfwd \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/tuple \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/cassert \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/atomic \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/climits \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/stdexcept \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__split_buffer \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/v8-version.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/v8config.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/JavaObject.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/EventEmitter.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/NativeObject.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/AndroidUtil.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/stdlib.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/EventEmitter.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/JNIUtil.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/AndroidUtil.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/JSException.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/ProxyFactory.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/TypeConverter.h \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/map \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__tree \
+ /Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/functional \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/V8Util.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/org.appcelerator.kroll.KrollModule.h \
+ /Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/Proxy.h
+
+jni/me.direcciona.adwords.android.AdwordsandroidModule.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/Proxy.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/v8.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/stdint.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/stdio.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/wchar.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/wctype.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/xlocale.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/utility:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__config:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__tuple:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/cstddef:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/type_traits:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/vector:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__bit_reference:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/algorithm:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/initializer_list:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/cstring:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/string.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/memory:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/typeinfo:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/exception:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/cstdint:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/new:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/limits:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/iterator:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__functional_base:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/iosfwd:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/tuple:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/cassert:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/atomic:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/climits:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/stdexcept:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__split_buffer:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/v8-version.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/v8config.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/JavaObject.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/EventEmitter.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/NativeObject.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/AndroidUtil.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/../../android/support/include/stdlib.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/EventEmitter.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/JNIUtil.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/AndroidUtil.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/JSException.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/ProxyFactory.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/TypeConverter.h:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/map:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/__tree:
+
+/Users/mojotribe/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libcxx/include/functional:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/V8Util.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/org.appcelerator.kroll.KrollModule.h:
+
+/Users/mojotribe/Library/Application\ Support/Titanium/mobilesdk/osx/6.1.0.GA/android/native/include/Proxy.h:
